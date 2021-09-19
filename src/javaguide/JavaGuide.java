8a55ab3028ebf2a62c571625bc7b521a044705e9
Jose <jmejiamu@gmail.com>
2021-09-18 18:16:01 -0700

    first commit setup

---
+++ src/javaguide/JavaGuide.java
@@ -0,0 +1,24 @@
+
+package javaguide;
+
+/**
+ *
+ * @author JoseMejia62
+ */
+public class JavaGuide {
+
+    public static void main(String[] args) {
+        // -  VARIABLES
+        
+        String greeting = "Hello";
+        
+        int age = 23;
+        
+        boolean married =  false;
+        
+        final String name = "JoseMejia62";
+        
+        System.out.println(greeting+ ", " + name + "!");
+    }
+    
+}
