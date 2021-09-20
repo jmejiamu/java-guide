12e7e207cb774fab3233151465dad36a5fee8d0a
Jose <jmejiamu@gmail.com>
2021-09-19 18:35:37 -0700

    narrowing casting

--- src/javaguide/Casting.java
+++ src/javaguide/Casting.java
@@ -6,12 +6,20 @@ package javaguide;
  * @author JoseMejia62
  */
 public class Casting {
-    public static void CastingBasic(){
+    public static void wideningCasting(){
         int age = 20;
         
         double ageToDouble = age;
         
         System.out.println("Int - " + age);
         System.out.println("Casting age to double - " + ageToDouble);
+    }
+    
+    public static void narrowCasting(){
+        double total = 23.70d;
+        int totalToInt  =  (int) total;
+        
+        System.out.println("Double  - " + total);
+        System.out.println("Total to int - " + totalToInt);
     }
 }
